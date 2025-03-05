a36b418e2f4d122e728aad71accfab91b6faba41
Michał Motyl <majki@MacBook-Air-Micha.local>
2025-03-05 22:14:29 +0100

    add dashboard route

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db, login_manager
+from datetime import datetime
 from flask_login import UserMixin
 from flask_bcrypt import generate_password_hash, check_password_hash
 
@@ -17,3 +18,14 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    exercise = db.Column(db.String(100), nullable=False)
+    sets = db.Column(db.Integer, nullable=False)
+    reps = db.Column(db.Integer, nullable=False)
+    weight = db.Column(db.Float, nullable=True)  # Opcjonalna waga
+    
+    user = db.relationship('User', backref='workouts', lazy=True)
